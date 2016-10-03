1c58fafa979c822acc58555418ec3ea7f676b68a
Amantey <shurik1910@yandex.by>
2016-10-03 21:25:55 +0300

    Начало
    Начало

---
+++ Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Prepod.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Prepod.Controllers
+{
+    public class StudentsController : Controller
+    {
+        // GET: Students
+        mydbEntities model = new mydbEntities();
+        public ActionResult Index()
+        {
+            List<students_group> students = model.students_group.ToList();
+            return View(students);
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Prepod.MvcApplication" Language="C#" %>
